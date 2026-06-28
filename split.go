09cc73aca5565eb994c0ccf97542adc815ff5069
Don Owens <don@regexguy.com>
2026-06-27 17:42:59 -0700

    added SplitOnDelims

--- split.go
+++ split.go
@@ -33,3 +33,18 @@ func SplitOnDelims(s string, delims []string, mergeDelim bool) []string {
 
 	return parts
 }
+
+// Splits the input string on any whitespace characters and returns a slice of
+// the resulting substrings. If the input string is empty, returns an empty
+// slice. If the input string does not contain any whitespace characters,
+// returns a slice containing the input string as the only element. If
+// mergeDelim is true, multiple consecutive whitespace characters are treated
+// as a single delimiter.
+//
+// The whitespace characters considered are: space, tab, newline, vertical tab,
+// form feed, carriage return, non-breaking space, and next line.
+func SplitOnSpace(s string, mergeDelim bool) []string {
+	delims := []string{" ", "\t", "\n", "\v", "\f", "\r", "\u0085",
+		"\u00a0"}
+	return SplitOnDelims(s, delims, mergeDelim)
+}
--- split_test.go
+++ split_test.go
@@ -71,3 +71,52 @@ func TestSplitOnDelims(t *testing.T) {
 		})
 	}
 }
+
+func TestSplitOnSpace(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      string
+		mergeDelim bool
+		expected   []string
+	}{
+		{
+			name:       "Empty input string",
+			input:      "",
+			mergeDelim: false,
+			expected:   []string{""},
+		},
+		{
+			name:       "No whitespace in input string",
+			input:      "hello-world",
+			mergeDelim: false,
+			expected:   []string{"hello-world"},
+		},
+		{
+			name:       "Single whitespace",
+			input:      "hello world",
+			mergeDelim: false,
+			expected:   []string{"hello", "world"},
+		},
+		{
+			name:       "Multiple whitespaces",
+			input:      "hello  world\tthis\nis\va\ftest\r\u0085\u00a0next line",
+			mergeDelim: false,
+			expected: []string{"hello", "", "world", "this", "is", "a",
+				"test", "", "", "next", "line"},
+		},
+		{
+			name:       "Merge consecutive whitespaces",
+			input:      "hello  world\tthis\nis\va\ftest\r\u0085\u00a0next line",
+			mergeDelim: true,
+			expected: []string{"hello", "world", "this", "is", "a", "test",
+				"next", "line"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := stringutil.SplitOnSpace(tt.input, tt.mergeDelim)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
